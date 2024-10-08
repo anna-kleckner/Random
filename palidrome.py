1d86e9cd747c5aac6d30440093505ebb1e53d9bd
Anna <annakleckner08@gmail.com>
2024-10-08 11:45:36 -0500

    Add files via upload

---
+++ palidrome.py
@@ -0,0 +1,14 @@
+def main():
+    test = str(input("Potential Palindrome: "))
+    lowercase = test.lower()
+    simple_string = ''.join([*filter(str.isalnum, lowercase)])
+    backwards_string = simple_string[::-1]
+
+    print(backwards_string)
+    
+    if simple_string == backwards_string:
+        print(f"{test} is a palindrome")
+    else:
+        print(f"'{test}' is not a palindrome")
+
+main()
